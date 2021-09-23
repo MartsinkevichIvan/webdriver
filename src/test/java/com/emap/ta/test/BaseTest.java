498205077631c1e173b6d380e3a043c67e5a71bd
New Sheriff in Town <TrainingEmp@tut.by>
2021-09-23 20:14:03 +0300

    add aftersuite

--- src/test/java/com/emap/ta/test/BaseTest.java
+++ src/test/java/com/emap/ta/test/BaseTest.java
@@ -3,6 +3,7 @@ package com.emap.ta.test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
@@ -17,13 +18,13 @@ public class BaseTest {
         driver.manage().window().maximize();
     }
 
-//    @AfterSuite
-//    public void afterSuite(){
-//        if(driver!=null){
-//            driver.close();
-//            driver.quit();
-//        }
-//    }
+    @AfterSuite
+    public void afterSuite(){
+        if(driver!=null){
+            driver.close();
+            driver.quit();
+        }
+    }
 
     public WebDriver getDriver(){
         return driver;
